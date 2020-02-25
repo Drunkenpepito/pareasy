af8427eeb03d54faf1eb69c7bd04af13477f9e4c
pierre furic <pfuric@hotmail.com>
2020-02-25 11:58:25 +0100

    demo index for betrooms

--- app/controllers/bet_rooms_controller.rb
+++ app/controllers/bet_rooms_controller.rb
@@ -1,23 +1,11 @@
 class BetRoomsController < ApplicationController
   def index
-    @bet_rooms = policy_scope(Bet_room)
+    @bet_rooms = policy_scope(BetRoom)
+
   end
 
   def show
   end
 
-  def new
-  end
 
-  def create
-  end
-
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
-  end
 end
