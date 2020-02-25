9325aba6ba04f30abbd3ee3f5939681d848dde19
Edouard Castelnau <edouard.castelnau@gmail.com>
2020-02-25 12:58:06 +0100

    bet_room_new

--- app/controllers/bet_rooms_controller.rb
+++ app/controllers/bet_rooms_controller.rb
@@ -9,9 +9,26 @@ class BetRoomsController < ApplicationController
   end
 
   def new
+    @bet_room = BetRoom.new
+    authorize @bet_room
+
   end
 
   def create
+    @bet_room = BetRoom.new(bet_room_params)
+    # @star.user = current_user
+    authorize @bet_room
+    if @bet_room.save
+      redirect_to bet_room_path(@bet_room)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def bet_room_params
+    params.require(:bet_room).permit(:name, :photo)
   end
 
 end
