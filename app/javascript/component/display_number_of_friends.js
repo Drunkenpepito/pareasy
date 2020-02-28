8566f924b122ce598a771e0464bb001fbd9b13dc
Xavier Blanchy <blanchyx@gmail.com>
2020-02-28 12:26:48 +0100

    seed + counter

---
+++ app/javascript/component/display_number_of_friends.js
@@ -0,0 +1,15 @@
+const displayNumberOfFriends = () => {
+  const friendsCheckBoxes = document.querySelectorAll('.friends-input input')
+
+  friendsCheckBoxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', (e) => {
+      const counter = document.querySelector("#friends-counter")
+      const checkedInputs = document.querySelectorAll('.friends-input input:checked')
+      counter.innerHTML = checkedInputs.length
+      console.log(checkedInputs.length);
+    })
+  })
+
+}
+
+export { displayNumberOfFriends };
