8039227ccf8c2e5adc85559771fb40f0a2e0ac44
pierre furic <pfuric@hotmail.com>
2020-03-03 11:09:16 +0100

    Resolve conflicts

--- db/migrate/20200302150959_add_column_to_chatrooms.rb
+++ db/migrate/20200302150959_add_column_to_chatrooms.rb
@@ -1,6 +1,5 @@
 class AddColumnToChatrooms < ActiveRecord::Migration[6.0]
   def change
-    remove_column :chatrooms, :bet_room_id
     add_reference :chatrooms, :bet_room, foreign_key: true
   end
 end
